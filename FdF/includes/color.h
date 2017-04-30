b795399f14523fd9367d914a57274f2eeb368412
Kevin Costa <kcosta@student.42.fr>
2017-04-30 13:02:22 +0200

    Color Implemented

---
+++ FdF/includes/color.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kcosta <kcosta@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/30 12:26:43 by kcosta            #+#    #+#             */
+/*   Updated: 2017/04/30 12:39:23 by kcosta           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOR_H
+# define COLOR_H
+
+typedef unsigned char	t_uchar;
+
+typedef struct	s_color
+{
+	t_uchar		r;
+	t_uchar		g;
+	t_uchar		b;
+}				t_color;
+
+t_color			new_color(t_uchar r, t_uchar g, t_uchar b);
+
+#endif
